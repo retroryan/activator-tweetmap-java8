05ebc8a9327b6ec83cc690ef0e0ab25f7b71a0a3
retroryan <ryan@grandcloud.com>
2014-06-05 12:46:35 -0600

    Fixing the starting state of the tutorial.

--- app/controllers/Tweets.java
+++ app/controllers/Tweets.java
@@ -1,20 +1,18 @@
 package controllers;
 
-import actors.UserActor;
 import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.F.Promise;
+import play.libs.Json;
 import play.libs.ws.WS;
 import play.libs.ws.WSResponse;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
-import play.mvc.WebSocket;
 
 public class Tweets extends Controller {
-
 
     public static Result index() {
         return ok(views.html.index.render("TweetMap"));
     }
-
 
 }
